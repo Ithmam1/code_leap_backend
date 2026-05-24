1bf64b63cbbee4beadeebebe98346e266c62a922
Ithmam1 <rafiithmamhussain@gmail.com>
2026-05-25 01:00:12 +0600

    fixed csrf

--- leap/settings.py
+++ leap/settings.py
@@ -31,6 +31,9 @@ DEBUG = config('DEBUG',  cast=bool)
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-e327e.up.railway.app',
+]
 
 
 # Application definition
